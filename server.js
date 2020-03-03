d584c6d6eb010e29386f1064706029a058338c23
Anas Qazi <ianasqazi@gmail.com>
2020-03-03 13:51:41 -0500

    testing front

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 // Load env variables
 dotenv.config({ path: path.join(__dirname, 'config/config.env') });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
@@ -15,11 +15,18 @@ app.use(cors());
 // Use development logging middleware
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
-app.get('/', (req, res, next) => {
-  res.status(200).json({
-    data: 'Hello from J&D!'
-  });
+app.get('/', (req, res) => {
+  // res.status(200).json({
+  //   data: 'Hello from J&D!'
+  // });
+    res.sendFile(path.join(__dirname, "client/public", "index.html"));
+
 });
+
+// app.use((_, res) => {
+//   res.sendFile(join(__dirname, "public", "index.html"));
+// });
+
 
 app.listen(
   PORT,
