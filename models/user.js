d70cbd0e363534b7ed537abfea78319054df4d3a
David Gehtman <davidgehtman.91@gmail.com>
2020-03-04 15:42:00 -0500

    creating user Schema

--- models/user.js
+++ models/user.js
@@ -7,5 +7,27 @@ var User = mongoose.model('User', {
   created: Date
 });
 
+// create a user schema 
+
+const UserSchema = new mongoose.Schema({
+gitHub:{
+  username: String,
+  picture:String,
+  email: String,
+},
+resume:{
+  contact: String,
+  experience: String,
+  education: String,
+  skills: String,
+  objective:String
+},
+coverLetter:{
+  title: String,
+  body:String,
+},
+
+})
+
 
 module.exports = User;
