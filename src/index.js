bf414d68c3438f2b10b0b05066987c65cebd1e79
Khalil Mouna <11007446+KhalilMouna@users.noreply.github.com>
2020-03-04 14:21:56 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import { Auth0Provider } from "./react-auth0-spa";
-// import config from "./auth_config.json.js";
-import config from "./auth_config.json.js";
+import config from "./auth_config.json";
 import history from "./utils/history";
 
 const onRedirectCallback = appState => {
