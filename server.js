fbcff3eedba686cb7250c0d9b40cc05d95e02a3d
Sal Tamay <sal.tamay@outlook.com>
2020-03-07 12:56:28 -0500

    Delete catch all route and serve only static files

--- server.js
+++ server.js
@@ -19,12 +19,6 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (_req, res) => {
-	res.sendFile(path.join(__dirname + 'client/build/index.html'));
-});
-
 const PORT = process.env.PORT || 3001;
 
 app.listen(
