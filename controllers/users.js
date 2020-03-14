28e3e2efeb3d91011678ab5d7bfc0ee752606c6c
Sal Tamay <sal.tamay@outlook.com>
2020-03-14 15:53:54 -0400

    fix: breaking route changes

--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,39 @@
-const db = require("../models");
+const db = require('../models');
 
 module.exports = {
   findAll: function(req, res) {
-    db.User
-      .find(req.query)
+    db.User.find(req.query)
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findById: function(req, res) {
-    db.User
-      .findById(req.params.id)
+    db.User.findById(req.params.id)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findByEmail: function(req, res) {
-    db.User
-      .findOne({ email: req.params.email })
-      .then(dbModel => res.json(dbModel))
+    db.User.findOne({ email: req.params.email })
+      .then(user =>
+        res.json({
+          success: true,
+          data: user
+        })
+      )
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    db.User
-      .create(req.body)
-      .then(dbModel => res.json(dbModel))
+    db.User.create(req.body)
+      .then(user =>
+        res.json({
+          success: true,
+          data: user
+        })
+      )
       .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
-    db.User
-      .findOneAndUpdate({ id: req.params.id }, req.body)
+    db.User.findOneAndUpdate({ id: req.params.id }, req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
