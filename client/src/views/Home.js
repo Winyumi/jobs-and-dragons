029fc99e069f31f0f259ab49d4e0adcd4a5552cb
Khalil Mouna <khalilmouna@Naomi.local>
2020-03-14 13:00:04 -0400

    update

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { Component } from 'react';
+// import { Redirect } from 'react-router-dom';
 
-import Hero from "../components/Hero";
+function Home() {
+  return (
+    <React.Fragment>
+      <h1 style={headerStyle}>Jobs & Dragons</h1>
+      <p> 
+        I am determined, capable, and hard-working and I love to rise to a challenge. <br/> 
+        I thoroughly enjoy working with people and am happy to find any place in the team. <br/> 
+        I've worked in a variety of positions in a large range of fields and I know I can always find a great way to contribute to a project. <br/>
+      </p>
+    </React.Fragment>
+  )
+}
 
-const Home = () => (
-    <Hero />
-);
+const headerStyle = {
+    background: '#333',
+    color: '#fff',
+    textAlign: 'center',
+    position: 'fixed',
+    top: '0px',
+    width: '100%',
+    padding: '5px'
+  }
 
 export default Home;
