1db4fc1c53454e1461906075360aaea00f8cbda1
Winyumi <winguy2003@hotmail.com>
2020-03-14 13:21:28 -0400

    add findByEmail

--- controllers/users.js
+++ controllers/users.js
@@ -14,6 +14,12 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  findByEmail: function(req, res) {
+    db.User
+      .findById({ email: req.params.email })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
   create: function(req, res) {
     db.User
       .create(req.body)
