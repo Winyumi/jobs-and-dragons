dbd0b8dcbb1f0bb51d812a918a0022a7a8c4e9c2
Sal Tamay <sal.tamay@outlook.com>
2020-03-14 17:52:04 -0400

    Merge: Change a elements with Link component

--- client/src/views/Profile.js
+++ client/src/views/Profile.js
@@ -1,13 +1,11 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useUserContext } from '../contexts/UserContext';
 import { api } from '../utils/api';
 
-
 import 'materialize-css';
-import "./profileStyles.css";
+import './profileStyles.css';
 
-
-import Highlight from '../components/Highlight';
 import Loading from '../components/Loading';
 import { useAuth0 } from '../react-auth0-spa';
 
@@ -34,28 +32,37 @@ const Profile = () => {
   }, [loading, user, dispatch]);
 
   return (
+    <div className='row'>
+      <div className='center col s12 m6'>
+        <img
+          src={user.picture}
+          alt='User Profile'
+          className='circle responsive-img'
+          id='userImage'
+        />
+        <h3>USERNAME</h3>
+        <div className='card-panel grey'>{user.name}</div>
+      </div>
 
-  <div class="row">
-
-    <div class="center col s12 m6">
-      <img src={user.picture} alt='User Profile Picture' class="circle responsive-img" id="userImage"/>
-      <h3>USERNAME</h3>
-      <div class="card-panel grey">{ user.name }</div>
+      <div className='center col s12 m6'>
+        <h3>Begin Your QUEST</h3>
+        <Link className='btn-large' to='/game' name='gameBtn'>
+          Quest 1
+        </Link>
+        <br></br>
+        <Link className='btn-large' to='#' name='gameBtn'>
+          Quest 2
+        </Link>
+        <br></br>
+        <Link className='btn-large' to='#' name='gameBtn'>
+          Quest 3
+        </Link>
+        <br></br>
+        <Link className='btn-large' to='#' name='gameBtn'>
+          Quest 4
+        </Link>
+      </div>
     </div>
-
-    <div class="center col s12 m6">
-      <h3>Begin Your QUEST</h3>
-      <a class='btn-large' href='/game' type='submit' name='gameBtn'>GAME</a>
-      <br></br>  
-      <a class="btn-large" href='#' type='submit' name='gameBtn'>RESUME</a>
-      <br></br>
-      <a class="btn-large" href='#' type='submit' name='gameBtn'>Quest 3</a>
-      <br></br>
-      <a class="btn-large" href='#' type='submit' name='gameBtn'>Quest 4</a>
-    </div>
-
-  </div>
-
   );
 };
 
