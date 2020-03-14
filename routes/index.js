136cc56c40833193a48662fc004f8e65c615d142
Winyumi <winguy2003@hotmail.com>
2020-03-14 13:20:28 -0400

    add v1 to api route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 // API Routes
-router.use("/api", apiRoutes);
+router.use("/api/v1", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
