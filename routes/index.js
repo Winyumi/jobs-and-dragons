184cfbdf7da06e17634c21d8c843fde40a601d87
Winyumi <winguy2003@hotmail.com>
2020-03-14 13:20:11 -0400

    fix catchall pointer

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,11 @@ router.use("/api", apiRoutes);
 
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
+  if (process.env.NODE_ENV === 'production') {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  } else {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  }
 });
 
 module.exports = router;
