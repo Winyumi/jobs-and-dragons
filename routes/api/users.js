c87f282fb5005f0fc68baaeac4cdcb29a8d27284
Winyumi <winguy2003@hotmail.com>
2020-03-14 13:23:29 -0400

    hotfix - change fetch to email

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,10 +6,18 @@ router.route("/")
   .get(usersController.findAll)
   .post(usersController.create);
 
+/*
 // Matches with "/api/v1/users/:id"
 router
   .route("/:id")
   .get(usersController.findById)
+  .put(usersController.update);
+*/
+
+// Matches with "/api/v1/users/:email"
+router
+  .route("/:email")
+  .get(usersController.findByEmail)
   .put(usersController.update);
 
 module.exports = router;
