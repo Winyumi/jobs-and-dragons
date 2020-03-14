20bc283dd59c5dbae63bbaf51d75d1f6bb25ee72
Winyumi <winguy2003@hotmail.com>
2020-03-14 13:20:53 -0400

    minor edit

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ router.use("/api/v1", apiRoutes);
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
   if (process.env.NODE_ENV === 'production') {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   } else {
     res.sendFile(path.join(__dirname, "../client/public/index.html"));
   }
