dcff1acdc6af3d8f7daba9f9e088a32a8b6a0242
Anas Qazi <ianasqazi@gmail.com>
2020-04-27 17:25:14 -0400

    updated job listing styling

--- client/src/views/jobListing.js
+++ client/src/views/jobListing.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
-import dotenv from 'dotenv';
+import React, { Component } from "react";
+import dotenv from "dotenv";
 import Auth0Context from "../react-auth0-spa";
-import 'materialize-css';
-import dateFormat from 'dateformat';
-import Loading from '../components/Loading';
-import SavedJobs from '../components/SavedJobs/index';
+import "materialize-css";
+import dateFormat from "dateformat";
+import Loading from "../components/Loading";
+import SavedJobs from "../components/SavedJobs/index";
+
+import backgroundDark from "../assets/dark-honeycomb.png";
 
 dotenv.config();
 
@@ -12,178 +14,196 @@ const APP_ID = "a69247c0";
 const APP_KEY = "24fc9762a9d2f3a031f002f7afe14f75";
 
 export default class jobListing extends Component {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        items: [],
-        query: ''
-      };
-    }
-    static contextType = Auth0Context; 
-    componentDidMount() {
-
-        fetch("https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=" +
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+      query: "",
+    };
+  }
+  static contextType = Auth0Context;
+  componentDidMount() {
+    fetch(
+      "https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=" +
         APP_ID +
         "&app_key=" +
         APP_KEY
     )
-
-        .then(res => res.json())
-        .then(
-
-            (result) => {
-                // console.log(result);
-              this.setState({
-                isLoaded: true,
-                items: result.results,
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error
-              });
-            }
-          )
-    }
-
-    handleSubmitSave = (item) => {
-      const jobInfo={
-        id:item.id,
-        title:item.title,
-        description:item.description,
-        url:item.redirect_url,
-        company:item.company.display_name,
-        location:{
-          area:item.location.area,
-          latitude: item.latitude,
-          longitude:item.longitude
-      }
-      }
-      
-      const userEmail = this.context.user.email;
-  
-      async function updateJobInfo(data, email) {
-        const res = fetch(`/api/v1/users/emailjs/${email}`, {
-          method: 'PUT',
-          mode: 'cors',
-          cache: 'no-cache',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(data)
-        });
-          if (res.ok) {
-          return res.data;
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
         }
+      );
+  }
+
+  handleSubmitSave = (item) => {
+    const jobInfo = {
+      id: item.id,
+      title: item.title,
+      description: item.description,
+      url: item.redirect_url,
+      company: item.company.display_name,
+      location: {
+        area: item.location.area,
+        latitude: item.latitude,
+        longitude: item.longitude,
+      },
+    };
+
+    const userEmail = this.context.user.email;
+
+    async function updateJobInfo(data, email) {
+      const res = fetch(`/api/v1/users/emailjs/${email}`, {
+        method: "PUT",
+        mode: "cors",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      if (res.ok) {
+        return res.data;
       }
-  
-        updateJobInfo(jobInfo,userEmail);
-    
     }
-  
 
-    handleSubmitSearch = (e) => {
-        e.preventDefault();
+    updateJobInfo(jobInfo, userEmail);
+  };
 
-        const query=this.state.query;
+  handleSubmitSearch = (e) => {
+    e.preventDefault();
 
-        fetch(
-          "https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=" +
-            APP_ID +
-            "&app_key=" +
-            APP_KEY +
-            "&what=" +
-            query
-        )
-          .then((res) => res.json())
-          .then(
-            (result) => {
-              this.setState({
-                isLoaded: true,
-                items: result.results,
-              });
-            },
-            (error) => {
-              this.setState({
-                isLoaded: true,
-                error,
-              });
-            }
-          );
-      };
-    
+    const query = this.state.query;
 
-    render() {
-      const { error, isLoaded, items } = this.state;
-      if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div><Loading /></div>;
-      } else {
-        return (
+    fetch(
+      "https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=" +
+        APP_ID +
+        "&app_key=" +
+        APP_KEY +
+        "&what=" +
+        query
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.results,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  };
 
-            <div className='row'>
-
-                <div className='center col s12 m3'>
-                    <h4>JOB LISTINGS</h4>
-                    <div className='center input-field'>
-                        <i className="large material-icons prefix">work</i>
-                        <input id="searchBox" 
-                        value={this.state.query} 
-                        type="text" 
-                        onChange={e => this.setState({query:e.target.value})} >
-                        </input>
-                        <label className="active">Search Job Title</label>
-                    </div>
-                    <input
-                    type='submit'
-                    value='SEARCH'
-                    className='btn brown darken-4'
-                    onClick= { e => this.handleSubmitSearch(e) }
-                    />
-                    <SavedJobs/>
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return (
+        <div>
+          <Loading />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div style={ListingStyles}>
+            <div className="row">
+              <div className="center col s12 m3">
+                <h4 style={{ color: "red" }}>JOB LISTINGS</h4>
+                <div className="center input-field">
+                  <input
+                    id="searchBox"
+                    value={this.state.query}
+                    type="text"
+                    onChange={(e) => this.setState({ query: e.target.value })}
+                    style={inputBoxStyle}
+                  ></input>
                 </div>
+                <input
+                  type="submit"
+                  value="SEARCH"
+                  className="btn btn-large red darken-4"
+                  onClick={(e) => this.handleSubmitSearch(e)}
+                />
+                <SavedJobs />
+              </div>
 
-                <div className='center col s12 m8'>
-                    <ul>
-                    {items.map(item => (
-                        <li key={ item.id }>
+              <div className="center col s12 m8">
+                <ul>
+                  {items.map((item) => (
+                    <li key={item.id}>
+                      <div className="card grey lighten-2">
+                        <div className="card-content">
+                          <h6 className="card-title">{item.title}</h6>
+                          <p>
+                            {" "}
+                            <b>Company :</b> {item.title}{" "}
+                          </p>
+                          <p>
+                            {" "}
+                            <b>Date :</b>{" "}
+                            {dateFormat(item.created, "dddd, mmmm dS, yyyy")}
+                          </p>
+                          <p>
+                            {" "}
+                            <b>Description :</b> {item.description}
+                          </p>
+                        </div>
 
-                            <div className="card brown lighten-3">
-
-                                <div className="card-content">
-                                    <h6 className="card-title activator">{ item.title }<i className="material-icons right">more_vert</i></h6>
-                                    <p> <b>Company :</b> { item.title } </p>
-                                    <p> <b>Date :</b> { dateFormat( item.created , "dddd, mmmm dS, yyyy") }</p>
-                                </div>
-
-                                <div className="card-action">
-                                    <a href={ item.redirect_url} target='_blank' className="btn brown darken-4">Apply</a>
-                                    <button onClick ={(e)=>this.handleSubmitSave(item)}
-                                     value="Save" className="btn brown darken-4">Save</button>
-                                </div>
-
-                                <div className="card-reveal brown darken-4">
-                                    <span className="card-title brown darken-4 brown lighten-3"><i className="material-icons right">close</i></span>
-                                    <p className="brown lighten-3"> <b>Category :</b> { item.category.label }</p>
-                      
-                                    <p className="brown lighten-3"> <b>Description :</b> { item.description }</p>
-
-
-                                </div>
-
-                            </div>
-                        </li>
-                    ))}     
-                    </ul>    
-                </div>
+                        <div className="card-action">
+                          <a
+                            href={item.redirect_url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="btn red darken-4"
+                          >
+                            Apply
+                          </a>
+                          <button
+                            onClick={(e) => this.handleSubmitSave(item)}
+                            value="Save"
+                            className="btn red darken-4"
+                          >
+                            Save
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-    
-        );
-      }
+          </div>
+        </>
+      );
     }
   }
+}
+
+const ListingStyles = {
+  backgroundColor: "black",
+  backgroundImage: `url(${backgroundDark})`,
+};
+
+const inputBoxStyle = {
+  backgroundColor: "white",
+};
