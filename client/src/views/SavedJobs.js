1eca99efe1b354eff91d2f4b159b300be1e1e4d5
Anas Qazi <ianasqazi@gmail.com>
2020-04-29 06:12:07 -0400

    updated background style for job listing and saved listing

--- client/src/views/SavedJobs.js
+++ client/src/views/SavedJobs.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import 'materialize-css';
-import SavedJobs from '../components/SavedJobs/index';
+import React from "react";
+import "materialize-css";
+import SavedJobs from "../components/SavedJobs/index";
+
+import backgroundDark from "../assets/dark-honeycomb.png";
 
 const SavedJobsView = () => {
   return (
-    <div className='row'>
-      <div className='center col s12'>
-        <SavedJobs/>
+    <div className="row" style={ListingStyles}>
+      <div className="center col s12">
+        <SavedJobs />
       </div>
     </div>
   );
-}
+};
 
 export default SavedJobsView;
+
+const ListingStyles = {
+  backgroundImage: `url(${backgroundDark})`,
+  height: "100vh",
+  margin: "0px",
+};
