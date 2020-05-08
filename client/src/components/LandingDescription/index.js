5026a73e2fe83d528f879fe136ee49a81b6996e9
Anas Qazi <ianasqazi@gmail.com>
2020-05-08 17:23:07 -0400

    adding component description to landing page

---
+++ client/src/components/LandingDescription/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import "materialize-css";
+
+import background from "../../assets/light_honeycomb.png";
+import worldMap from "../../assets/J&D_World.png";
+
+export default class LandingDescription extends React.Component {
+  // componentDidMount() {}
+
+  render() {
+    return (
+      <>
+        <div style={descStyle}>
+          <div className="row">
+            <div className="center col s12 m6" style={{ marginTop: "50px" }}>
+              <h2>
+                Securing a job in the web development industry can be very
+                challenging.
+              </h2>
+              <br></br>
+              <br></br>
+              <h3>
+                Having been recipients of the Bootcamp’s Career Services, we
+                realize how much work and effort is needed to become competitive
+                candidates.
+              </h3>
+            </div>
+
+            <div className="center col s12 m6" style={{ marginTop: "50px" }}>
+              <img
+                src={worldMap}
+                alt="World Map"
+                className="responsive-img"
+                style={imageStyle}
+              />
+            </div>
+          </div>
+
+          <div className="row" style={descStyle}>
+            <div className="center col s12 m6" style={{ marginTop: "50px" }}>
+              <img
+                src={worldMap}
+                alt="World Map"
+                className="responsive-img"
+                style={imageStyle}
+              />
+            </div>
+            <div className="center col s12 m6" style={{ marginTop: "50px" }}>
+              <h3>
+                This process can be repetitive and arduous. What if there was a
+                way to make searching for a job, building career materials, and
+                keeping coding skills sharp a fun and engaging experience?
+              </h3>
+            </div>
+          </div>
+
+          <div className="row" style={descStyle}>
+            {/* <div className="center col s12 m6" style={{ marginTop: "50px" }}>
+              <img
+                src="../../assets/dark-honeycomb.png"
+                alt="World Map"
+                className="responsive-img"
+                style={imageStyle}
+              />
+            </div> */}
+            <div
+              className="center col s12"
+              style={{ marginTop: "50px", marginBottom: "200px" }}
+            >
+              <h3>
+                Jobs & Dragons is a FULL STACK application that reproduces the
+                methods necessary to become employed and infuses them into a
+                dungeon crawler/RPG game. Through GitHub account authentication,
+                the user is provided with their initial character stats which
+                improve as they finish quests. Each quest reflects a step in
+                preparing for entering the web development job market, including
+                creating a resume, searching for a job online, and crafting a
+                cover letter.
+              </h3>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+const descStyle = {
+  backgroundImage: `url(${background})`,
+  backgroundRepeat: "repeat",
+  backgroundPosition: "center",
+  // backgroundSize: "cover",
+  position: "relative",
+  width: "100%",
+  height: "100%",
+};
+
+const imageStyle = {
+  width: "500px",
+  height: "600px",
+  marginTop: "50px",
+};
--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,69 +1,71 @@
-import React from 'react';
-import brandLogo from '../assets/J&D_newLogo.png';
+import React from "react";
 
-import background from '../assets/J&D_Dungeon_dark.jpg';
+import brandLogo from "../assets/J&D_newLogo.png";
+import background from "../assets/J&D_Dungeon_dark.jpg";
 
-import 'materialize-css';
+import LandingDescription from "../components/LandingDescription";
+
+import "materialize-css";
 
 function Home() {
   return (
-    <React.Fragment>
+    <>
       <div>
         <img
-          class='responsive-img'
+          class="responsive-img"
           style={bgStyle}
           src={background}
-          alt='Background'
+          alt="Background"
         />
         <div style={topStyle}>
-          <div className='container'>
+          <div className="container">
             <img
               style={brandLogoStyle}
-              className='responsive-img'
+              className="responsive-img"
               src={brandLogo}
-              alt='Brand Logo'
+              alt="Brand Logo"
             />
           </div>
 
-          <div className='container' style={descStyle}>
+          <div className="container" style={descStyle}>
             <h3>Let the JOB HUNT begin ...</h3>
           </div>
         </div>
+        <LandingDescription />
       </div>
-    </React.Fragment>
+    </>
   );
 }
 
 const bgStyle = {
-  backgroundImage: `url(${background})`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  position: 'relative',
-  // display: "cover",
-  width: '100%',
-  height: 'calc(100vh - 130px)',
+  backgroundImage: `url(${background}) fixed`,
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  position: "relative",
+  width: "100%",
+  height: "calc(100vh - 130px)",
 };
 
 const topStyle = {
-  position: 'absolute',
-  top: '100px',
-  width: '100%',
-  textAlign: 'center',
+  position: "absolute",
+  top: "100px",
+  width: "100%",
+  textAlign: "center",
 };
 
 const brandLogoStyle = {
-  width: '100%',
-  paddingTop: '100px',
+  width: "100%",
+  paddingTop: "100px",
 };
 
 const descStyle = {
-  color: 'white',
-  marginTop: '30px',
+  color: "white",
+  marginTop: "30px",
   background:
-    'radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(255,255,255,0.1310129915730337) 87%)',
-  width: '90vw',
-  textShadow: '2px 2px black',
+    "radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(255,255,255,0.1310129915730337) 87%)",
+  width: "90vw",
+  textShadow: "2px 2px black",
 };
 
 export default Home;
