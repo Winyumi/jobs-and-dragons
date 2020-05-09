2de2d07dd67da8d0fd0660f6c65935a7f9d951b0
Anas Qazi <ianasqazi@gmail.com>
2020-05-08 20:23:23 -0400

    added carousal

--- client/src/components/LandingDescription/index.js
+++ client/src/components/LandingDescription/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { Carousel, Slider, Slide, Caption } from "react-materialize";
 import "materialize-css";
 
 import background from "../../assets/light_honeycomb.png";
 // import background from "../../assets/dark-honeycomb.png";
 import worldMap from "../../assets/J&D_World.png";
 import gameMap1 from "../../assets/GameMap1.png";
+import gameMap2 from "../../assets/GameMap2.png";
+import gameMap3 from "../../assets/GameMap3.png";
 
 export default class LandingDescription extends React.Component {
   // componentDidMount() {}
@@ -30,6 +33,53 @@ export default class LandingDescription extends React.Component {
               <img src={worldMap} alt="World Map" className="responsive-img" />
             </div>
           </div>
+
+          <div className="row">
+            <div
+              className="col s12 m6"
+              style={{
+                margin: "30px",
+                padding: "50px",
+                border: "5px solid black",
+                marginBottom: "100px",
+              }}
+            >
+              <Slider
+                fullscreen={false}
+                options={{
+                  duration: 200,
+                  height: 500,
+                  indicators: true,
+                  interval: 3000,
+                }}
+              >
+                <Slide image={<img alt="" src={gameMap1} />}>
+                  <Caption placement="center">
+                    <h3>This is our big Tagline!</h3>
+                    <h5 className="light grey-text text-lighten-3">
+                      Here's our small slogan.
+                    </h5>
+                  </Caption>
+                </Slide>
+                <Slide image={<img alt="" src={gameMap2} />}>
+                  <Caption placement="left">
+                    <h3>Left Aligned Caption</h3>
+                    <h5 className="light grey-text text-lighten-3">
+                      Here's our small slogan.
+                    </h5>
+                  </Caption>
+                </Slide>
+                <Slide image={<img alt="" src={gameMap3} />}>
+                  <Caption placement="left">
+                    <h3>Left Aligned Caption</h3>
+                    <h5 className="light grey-text text-lighten-3">
+                      Here's our small slogan.
+                    </h5>
+                  </Caption>
+                </Slide>
+              </Slider>
+            </div>
+          </div>
         </div>
       </>
     );
@@ -37,7 +87,7 @@ export default class LandingDescription extends React.Component {
 }
 
 const imageBoxStyle = {
-  padding: "100px",
+  padding: "80px",
 };
 
 const spanStyle = {
