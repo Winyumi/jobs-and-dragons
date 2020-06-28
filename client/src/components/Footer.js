8b16a8c2919b4f41f91efb6d0566e767c6ca6903
Sal Tamay <sal.tamay@outlook.com>
2020-06-28 11:07:10 -0400

    Clean up the code in Footer.js

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,23 +3,19 @@ import React from 'react';
 function Footer() {
   return (
     <footer style={footerStyle} copyrights='copy 2015 Copyright Text'>
-      <div>
-        <h5>JOBS & DRAGONS © 2020</h5>
-      </div>
+      <h5>JOBS & DRAGONS © 2020</h5>
     </footer>
   );
 }
 
 const footerStyle = {
   position: 'relative',
-  bottom: '0px',
   width: '100%',
   background: '#333',
   color: '#fff',
   textAlign: 'center',
   textShadow: '2px 2px black',
-  padding: '16px',
-  maxHeight: 65,
+  padding: '12px',
   minHeight: 65,
 };
 
